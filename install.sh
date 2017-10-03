89cef727f51c8ad694745e6c68f9a37d6009f154
Ayumi <staracg@gmail.com>
2017-10-04 04:13:44 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -37,8 +37,8 @@ if $UPGRADE; then
     sudo pip install pip --upgrade
     
     # Install vim 
-    sudo apt-get remove vim vim-runtime gvim
-    sudo apt-get install libncurses5-dev libgnome2-dev libgnomeui-dev \
+    sudo apt-get -y remove vim vim-runtime gvim
+    sudo apt-get -y install libncurses5-dev libgnome2-dev libgnomeui-dev \
     libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
     libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
     python3-dev ruby-dev lua5.1 lua5.1-dev libperl-dev git
