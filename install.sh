d3a7a7d9f5ae4c152329b20015c4ca5e21509e8e
Ayumi <staracg@gmail.com>
2017-10-03 15:44:28 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -33,32 +33,8 @@ if $UPGRADE; then
     sudo apt-get -y dist-upgrade
     sudo apt-get -y autoremove
     
-    # Install vim from source
-    sudo apt-get install -y libncurses5-dev libgnome2-dev libgnomeui-dev \
-    libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
-    libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
-    python3-dev ruby-dev lua5.1 lua5.1-dev libperl-dev git
-    sudo apt-get remove -y vim vim-runtime
-    cd ~/
-    git clone https://github.com/vim/vim.git
-    cd ~/vim
-    ./configure --with-features=huge \
-            --enable-multibyte \
-            --enable-rubyinterp \
-            --enable-pythoninterp \
-            --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu \
-            --enable-python3interp \
-            --with-python3-config-dir=/usr/lib/python3.5/config-3.5m-x86_64-linux-gnu \
-            --enable-perlinterp \
-            --enable-luainterp \
-            --enable-gui=gtk2 --enable-cscope --prefix=/usr \
-    make VIMRUNTIMEDIR=/usr/share/vim/vim80
-    sudo make install
-    sudo apt-get install -y vim-runtime
-    sudo update-alternatives --install /usr/bin/editor editor /usr/bin/vim 1
-    sudo update-alternatives --set editor /usr/bin/vim
-    sudo update-alternatives --install /usr/bin/vi vi /usr/bin/vim 1
-    sudo update-alternatives --set vi /usr/bin/vim
+    # Install vim 
+    sudo apt-get install vim
 fi
 
 if $POWERLINE; then
@@ -89,12 +65,12 @@ if $DOT; then
     # Copy dot file
     sudo apt-get install -y build-essential cmake
     sudo apt-get install -y python-dev
-    cp ./.vimrc ~
-    cp ./.bashrc ~
-    cp ./.bash_profile ~
-    cp ./.screenrc ~
-    cp ./.gdbinit ~
-    cp ./.tmux.conf ~
+    cp .vimrc ~
+    cp .bashrc ~
+    cp .profile ~
+    cp .screenrc ~
+    cp .gdbinit ~
+    cp .tmux.conf ~
     # Install vim plugin
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
     vim +PluginInstall +qall
