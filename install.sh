5a98b51828262dfaec92a2a2f4a2fe5d40f89fe4
Ayumi <staracg@gmail.com>
2017-11-07 06:22:16 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -41,7 +41,7 @@ if $UPGRADE; then
     sudo apt-get -y install libncurses5-dev libgnome2-dev libgnomeui-dev \
     libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
     libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev \
-    python3-dev ruby-dev lua5.1 lua5.1-dev libperl-dev git
+    python3-dev ruby-dev lua5.1 lua5.1-dev libperl-dev git vim-nox
     cd ~
     git clone https://github.com/vim/vim.git
     cd vim
@@ -57,10 +57,8 @@ if $UPGRADE; then
                 --enable-gui=gtk2 \
                 --enable-cscope \
                 --prefix=/usr/local
-    make VIMRUNTIMEDIR=/usr/local/share/vim/vim80
-    sudo apt-get install checkinstall
+    sudo make VIMRUNTIMEDIR=/usr/local/share/vim/vim80
     cd ~/vim
-    sudo checkinstall
     sudo make install
     sudo update-alternatives --install /usr/bin/editor editor /usr/bin/vim 1
     sudo update-alternatives --set editor /usr/bin/vim
