4af48464e9b3621f92f831f2185aa662685ad016
Ayumi <staracg@gmail.com>
2017-11-07 18:14:55 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -49,14 +49,21 @@ if $UPGRADE; then
                 --enable-multibyte \
                 --enable-rubyinterp=yes \
                 --enable-pythoninterp=yes \
-                --with-python-config-dir=/usr/lib/python2.7/config \
+                --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu \
                 --enable-python3interp=yes \
-                --with-python3-config-dir=/usr/lib/python3.5/config \
+                --with-python3-config-dir=/usr/local/lib/python3.5/config-3.5m-x86_64-linux-gnu \
                 --enable-perlinterp=yes \
                 --enable-luainterp=yes \
-                --enable-gui=gtk2 \
+                --with-luajit \
+                --enable-gui=gtk3 \
                 --enable-cscope \
                 --prefix=/usr/local
+                --with-features=huge \
+                --enable-fontset \
+                --enable-largefile \
+                --disable-netbeans \
+                --enable-fail-if-missing
+                
     sudo make VIMRUNTIMEDIR=/usr/local/share/vim/vim80
     cd ~/vim
     sudo make install
