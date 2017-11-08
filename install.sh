bcdc40d7229563bf1eac81bfb62ef79bf09e6565
Ayumi <staracg@gmail.com>
2017-11-09 03:34:36 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -50,8 +50,6 @@ if $UPGRADE; then
                 --enable-rubyinterp=yes \
                 --enable-pythoninterp=yes \
                 --with-python-config-dir=/usr/lib/python2.7/config-x86_64-linux-gnu \
-                --enable-python3interp=yes \
-                --with-python3-config-dir=/usr/local/lib/python3.5/config-3.5m-x86_64-linux-gnu \
                 --enable-perlinterp=yes \
                 --enable-luainterp=yes \
                 --enable-gui=gtk3 \
